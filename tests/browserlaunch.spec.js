3b04d11c702d680cf8500c1e69e32f3522a5b30a
gauravRepo7648 <imgauravs5153@gmail.com>
2026-04-03 18:11:17 +0530

    removed one line

--- tests/browserlaunch.spec.js
+++ tests/browserlaunch.spec.js
@@ -5,5 +5,4 @@ test('My First Playwright Test', async({page})=>{
     await page.goto("https://www.google.com/");
     //await expect()
     expect (await page.title()).toContain("Google");
-     //Navigate to the desired browser.
 });
